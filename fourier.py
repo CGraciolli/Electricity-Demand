77cadfcdd62e4738a9a9e91896bd465d54945ab7
CGraciolli <cgraciolli@gmail.com>
2023-03-08 14:38:51 +0100

    graph labels

--- fourier.py
+++ fourier.py
@@ -3,7 +3,11 @@ from scipy.signal import periodogram
 import numpy as np
 import matplotlib.pyplot as plt
 from data import listValues
+from seaborn import pointplot
 
 x = np.array(listValues)
 fast_fourier = fft(x)
 
+f, pxx = periodogram(x)
+plt.semilogy(f, pxx)
+plt.show()
--- graphs.py
+++ graphs.py
@@ -2,5 +2,9 @@ from seaborn import pointplot
 import matplotlib.pyplot as plt
 from data import listValues, listHours
 
+##x axis markings missing
 pointplot(x=listHours, y=listValues)
+plt.ylabel("Real Demand")
+plt.xlabel("Time")
+plt.xticks([])
 plt.show()
