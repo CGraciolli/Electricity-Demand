f8927a62b940c028390d7a83c5e87c5b5c96c19f
CGraciolli <cgraciolli@gmail.com>
2023-03-08 14:16:55 +0100

    fourier

--- fourier.py
+++ fourier.py
@@ -1,5 +1,9 @@
 from scipy.fft import fft
+from scipy.signal import periodogram
 import numpy as np
 import matplotlib.pyplot as plt
+from data import listValues
 
+x = np.array(listValues)
+fast_fourier = fft(x)
 
