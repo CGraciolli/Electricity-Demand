83a93d1e7f28302e7ed9a9524a1e173335a8f1f9
CGraciolli <cgraciolli@gmail.com>
2023-03-11 13:19:04 +0100

    graphs with titles

--- graphs.py
+++ graphs.py
@@ -4,7 +4,6 @@ from functions import getList, getData
 from decouple import config
 from scipy.signal import welch
 
-##missing titles for the graphs
 
 startDate = "2018-09-02T00:00:00+00:00"
 endDate = "2018-10-06T23:59:59+00:00"
@@ -19,17 +18,20 @@ for index, time in enumerate(listHours):
         month = time[5:7]
         listLabels.append(f"{month}/{day}")
     else:
-         listLabels.append(time)
+        listLabels.append(time)
 ax = pointplot(x=listLabels, y=listValues)
 for i, label in enumerate(ax.get_xticklabels()):
         if i % 24 != 0:
             label.set_visible(False)
-plt.ylabel("Real Demand")
-plt.xlabel("Time")
+plt.title("Real Electricity Demand in Spain", fontsize=20)
+plt.ylabel("Real Demand in MW")
 plt.xticks(rotation = 90)
 plt.show()
 
 ##periodogram
 f, pxx = welch(listValues)
+plt.ylabel("Real Demand")
+plt.xlabel("Frequency")
 plt.plot(f, pxx)
+plt.title("Periodogram for the Real Demand Electricity in Spain", fontsize=20)
 plt.show()
